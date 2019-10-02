c896e9ed36febf5c98a8132f8c391adde0f50afd
Ryan Mulligan <rmulligan@elevatedthird.com>
2019-10-01 23:33:14 -0600

    Update styles and js

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const handleMenuItemClick = (item) => {
   item.addEventListener('click', () => document.body.classList.remove(activeClass))
 }
 
-const addMenuItemEvent = (menu) => {
+const setupmM = (menu) => {
   [...menu.children].forEach(item => handleMenuItemClick(item));
 }
 
@@ -39,4 +39,4 @@ menuToggle.addEventListener('click', (e) => {
 
 setupHeadlineRefs(verbs.children);
 buildMenu(menu);
-addMenuItemEvent(menu);
+setupmM(menu);
